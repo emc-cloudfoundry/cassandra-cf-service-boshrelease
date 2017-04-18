cfda9e8c9d842dd80b53a550ada0695c67367cee
Omer MISSOUM <omer.missoum@orange.com>
2017-04-18 10:26:00 +0200

    adding cassandra stress

---
+++ jobs/cassandra_server/templates/bin/cassandra-stress.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e # exit immediately if a simple command exits with a non-zero status.
+set -u # report the usage of uninitialized variables.
+
+export LANG=en_US.UTF-8
+
+export CASSANDRA_BIN=/var/vcap/packages/cassandra/bin
+export CASSANDRA_CONF=/var/vcap/jobs/cassandra_server/conf
+export CASSANDRA_TOOL=/var/vcap/packages/cassandra/tools
+
+export JAVA_HOME=/var/vcap/packages/openjdk
+export PATH=$PATH:/var/vcap/packages/openjdk/bin:$CASSANDRA_BIN:$CASSANDRA_CONF:$CASSANDRA_TOOL
+
+## export CASSANDRA_CONF=/var/vcap/jobs/cassandra_server/conf
+
+pushd /var/vcap/packages/cassandra/tools
+exec chpst -u vcap:vcap /var/vcap/packages/cassandra/tools/cassandra-stress write n=10 
+popd
+exit 0
