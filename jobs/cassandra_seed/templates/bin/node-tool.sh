1f1229f90882ce8f5488c9fe25ea951c005f280f
Omer MISSOUM <omer.missoum@orange.com>
2017-04-19 12:26:07 +0200

    added arguments tools on seed node to v2

--- jobs/cassandra_seed/templates/bin/node-tool.sh
+++ jobs/cassandra_seed/templates/bin/node-tool.sh
@@ -6,12 +6,12 @@ set -u # report the usage of uninitialized variables.
 export LANG=en_US.UTF-8
 
 export CASSANDRA_BIN=/var/vcap/packages/cassandra/bin
-export CASSANDRA_CONF=/var/vcap/jobs/cassandra_server/conf
+export CASSANDRA_CONF=/var/vcap/jobs/cassandra_seed/conf
 
 export JAVA_HOME=/var/vcap/packages/openjdk
 export PATH=$PATH:/var/vcap/packages/openjdk/bin
 
-export CASSANDRA_CONF=/var/vcap/jobs/cassandra_server/conf
+export CASSANDRA_CONF=/var/vcap/jobs/cassandra_seed/conf
 
 pushd /var/vcap/packages/cassandra/bin
 if [ $# -eq 0 ];
