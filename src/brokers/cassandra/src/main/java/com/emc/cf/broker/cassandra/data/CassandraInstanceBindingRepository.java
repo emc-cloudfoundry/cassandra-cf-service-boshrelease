b0ff7974bb79fb5638adde26c3881f1124334637
Pierre Oblin <pierre.oblin@orange.com>
2017-07-21 11:24:20 +0000

    remove original brokers, src and packages

--- src/brokers/cassandra/src/main/java/com/emc/cf/broker/cassandra/data/CassandraInstanceBindingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emc.cf.broker.cassandra.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CassandraInstanceBindingRepository extends JpaRepository<CassandraInstanceBinding, Long> {
-  CassandraInstanceBinding findOneByBindingId(String bindingId);
-}
--- src/brokers/cassandra/src/main/java/com/emc/cf/broker/cassandra/data/CassandraInstanceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emc.cf.broker.cassandra.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CassandraInstanceRepository extends JpaRepository<CassandraInstance, Long> {
-  CassandraInstance findOneByServiceInstanceId(String serviceInstanceId);
-}
