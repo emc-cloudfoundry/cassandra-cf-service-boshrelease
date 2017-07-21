1e44c8bdd05d54a59a41e127c9a03b380278683d
Pierre Oblin <pierre.oblin@orange.com>
2017-07-21 18:23:02 +0000

    FIX: -D flags must be set before jar file in java command line
    fix broker
    clean bosh release

--- downloadblob.sh
+++ downloadblob.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-wget http://repo1.maven.org/maven2/org/eclipse/jetty/jetty-runner/9.4.0.v20161208/jetty-runner-9.4.0.v20161208.jar
 wget http://apache.trisect.eu/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz
