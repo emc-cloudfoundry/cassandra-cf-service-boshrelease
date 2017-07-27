9f4f80d8c0c2a2cd9b2d8f3a52db03b2ef1c21ae
Jean-Christophe LOSAPIO <jeanchristophe.losapio@orange.com>
2017-07-27 15:47:47 +0200

    update run.sh script (application.yml usage)

--- src/cassandra-open-service-broker/run.sh
+++ src/cassandra-open-service-broker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/cassandra-open-service-broker-0.0.1-SNAPSHOT.jar
+java -Dspring.config.location=file:target/application.yml -jar target/cassandra-open-service-broker-0.0.1-SNAPSHOT.jar
