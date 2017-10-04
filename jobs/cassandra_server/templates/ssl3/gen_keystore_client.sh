d14dbf30cdfa01903394761d3ffc64d96b614b8d
Omer MISSOUM <omer.missoum@orange.com>
2017-10-04 13:35:27 +0000

    evolution ssl cassandra39 v1.5

---
+++ jobs/cassandra_server/templates/ssl3/gen_keystore_client.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#set -x
+set -e # exit immediately if a simple command exits with a non-zero status.
+set -u # report the usage of uninitialized variables.
+export KEY_STORE_PATH=/var/vcap/jobs/cassandra_server/config/certs/
+cd ${KEY_STORE_PATH}
+export JAVA_HOME=/var/vcap/packages/openjdk
+export PATH=$PATH:$JAVA_HOME/bin:.
+
+export KEY_STORE="$KEY_STORE_PATH/${HOST_NAME}_cassandra.keystore"
+export PKS_KEY_STORE="$KEY_STORE_PATH/${HOST_NAME}_cassandra.pks12.keystore"
+export TRUST_STORE="$KEY_STORE_PATH/${HOST_NAME}_cassandra.truststore"
+export CLUSTER_NAME="<%=properties.cassandra_server.cluster_name%>"
+export CLUSTER_PUBLIC_CERT="$KEY_STORE_PATH/CLUSTER_${CLUSTER_NAME}_PUBLIC.cer"
+export CLIENT_PUBLIC_CERT="$KEY_STORE_PATH/CLIENT_${CLUSTER_NAME}_PUBLIC.cer"
+
+export HOST_NAME1=`hostname -I`
+export HOST_NAME=${HOST_NAME1//[[:space:]]}
+export STOREPASS="<%=properties.cassandra_server.cass_KSP%>"
+export KEYPASS=$STOREPASS
+export PASSWORD=$KEYPASS
+
+
+### Cluster key setup.
+# Create the cluster key for cluster communication.
+keytool -genkey -keyalg RSA -alias "${CLUSTER_NAME}_CLUSTER" -keystore "$KEY_STORE" -storepass "$PASSWORD" -keypass "$PASSWORD" \
+-dname "CN=${HOST_NAME} $CLUSTER_NAME cluster, OU=Orange, O=Orange, L=Lyon, C=FR" \
+-validity 36500
+
+# Create the public key for the cluster which is used to identify nodes.
+keytool -export -alias "${CLUSTER_NAME}_CLUSTER" -file "$CLUSTER_PUBLIC_CERT" -keystore "$KEY_STORE" \
+-storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
+
+# Import the identity of the cluster public cluster key into the trust store so that nodes can identify each other.
+keytool -import -v -trustcacerts -alias "${CLUSTER_NAME}_CLUSTER" -file "$CLUSTER_PUBLIC_CERT" -keystore "$TRUST_STORE" \
+-storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
+
+
+### Client key setup.
+# Create the client key for CQL.
+keytool -genkey -keyalg RSA -alias "${CLUSTER_NAME}_CLIENT" -keystore "$KEY_STORE" -storepass "$PASSWORD" -keypass "$PASSWORD" \
+-dname "CN=${HOST_NAME} $CLUSTER_NAME cluster, OU=Orange, O=Orange, L=Lyon, C=FR" \
+-validity 36500
+
+# Create the public key for the client to identify itself.
+keytool -export -alias "${CLUSTER_NAME}_CLIENT" -file "$CLIENT_PUBLIC_CERT" -keystore "$KEY_STORE" \
+-storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
+
+# Import the identity of the client pub  key into the trust store so nodes can identify this client.
+keytool -importcert -v -trustcacerts -alias "${CLUSTER_NAME}_CLIENT" -file "$CLIENT_PUBLIC_CERT" -keystore "$TRUST_STORE" \
+-storepass "$PASSWORD" -keypass "$PASSWORD" -noprompt
+
+
+
+keytool -importkeystore -srckeystore "$KEY_STORE" -destkeystore "$PKS_KEY_STORE" -deststoretype PKCS12 \
+-srcstorepass "$PASSWORD" -deststorepass "$PASSWORD"
+
+openssl pkcs12 -in "$PKS_KEY_STORE" -nokeys -out "${CLUSTER_NAME}_CLIENT.cer.pem" -passin pass:"$PASSWORD"
+openssl pkcs12 -in "$PKS_KEY_STORE" -nodes -nocerts -out "${CLUSTER_NAME}_CLIENT.key.pem" -passin pass:"$PASSWORD"
+
+
+exit 0
+
