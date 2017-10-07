3525ff969d030ffff136c876cf0bbbc540fc5f68
Omer MISSOUM <omer.missoum@orange.com>
2017-10-07 19:29:54 +0000

    evolution ssl cassandra39 v1.8

--- jobs/cassandra_seed/templates/bin/sec-sysauth.sh
+++ jobs/cassandra_seed/templates/bin/sec-sysauth.sh
@@ -6,14 +6,32 @@ set -u # report the usage of uninitialized variables.
 export LANG=en_US.UTF-8
 
 export CASSANDRA_BIN=/var/vcap/packages/cassandra/bin
-export CASSANDRA_CONF=/var/vcap/jobs/cassandra_server/conf
+export CASSANDRA_CONF=/var/vcap/jobs/cassandra_seed/conf
 
 export JAVA_HOME=/var/vcap/packages/openjdk
 export PATH=$PATH:/var/vcap/packages/openjdk/bin:$CASSANDRA_BIN:$CASSANDRA_CONF
 
 export CASS_PWD="<%=properties.cassandra_seed.cass_pwd%>"
-## export CASSANDRA_CONF=/var/vcap/jobs/cassandra_server/conf
-export CLIENT_SSL=<%properties.cassandra_seed.client_encryption_options.enabled%>
+## export CASSANDRA_CONF=/var/vcap/jobs/cassandra_seed/conf
+export CLIENT_SSL=<%=properties.cassandra_seed.client_encryption_options.enabled%>
+
+if ${CLIENT_SSL} == "true"
+then
+ /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc"  -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p cassandra --ssl 2>&1>/dev/null
+ if [[ "$?" == 1 ]]; then
+  /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc"  -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p $CASS_PWD --ssl 2>&1>/dev/null
+ fi
+ /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc"  -e "alter keyspace system_auth WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true" -u cassandra -p $CASS_PWD --ssl  2>&1>/dev/null
+ /var/vcap/jobs/cassandra_seed/bin/./node-tool.sh repair system_auth
+#exit 0
+else
+ /var/vcap/packages/cassandra/bin/cqlsh `hostname -I` -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p cassandra 2>&1>/dev/null
+ if [[ "$?" == 1 ]]; then
+  /var/vcap/packages/cassandra/bin/cqlsh `hostname -I` -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p $CASS_PWD 2>&1>/dev/null
+ fi
+ /var/vcap/packages/cassandra/bin/cqlsh `hostname -I` -e "alter keyspace system_auth WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true" -u cassandra -p $CASS_PWD  2>&1>/dev/null
+ /var/vcap/jobs/cassandra_seed/bin/./node-tool.sh repair system_auth
+fi
 
 if ${CLIENT_SSL} == "true"
 then
