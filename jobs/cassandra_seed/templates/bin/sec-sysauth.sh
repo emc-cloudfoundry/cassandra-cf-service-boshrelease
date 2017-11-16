d2aa9037c0e7008efff03b37a87455085a84e739
Benjamin Gandon <bgandon.ext@orange.com>
2017-11-16 17:52:10 +0000

    Raise wait delay to 30s + Add more debug output

--- jobs/cassandra_seed/templates/bin/sec-sysauth.sh
+++ jobs/cassandra_seed/templates/bin/sec-sysauth.sh
@@ -21,36 +21,34 @@ cass_ip="<%= spec.ip %>"
 cass_port="<%= p('cassandra_seed.native_transport_port') %>"
 attempts=0
 while ! nc -z "$cass_ip" "$cass_port"; do
-	attempts=$(($attempts + 1))
-	if [[ $attempts -ge $max_attempts ]]; then
-		echo "ERROR: could not reach cassandra on IP '$cass_ip' and TCP port '$cass_port' after '$max_attempts' attemps. Aborting." >&2
-		exit 1
-	fi
-	sleep 1
+    attempts=$(($attempts + 1))
+    if [[ $attempts -ge $max_attempts ]]; then
+        echo "ERROR: could not reach cassandra on IP '$cass_ip' and TCP port '$cass_port' after '$max_attempts' attemps. Aborting." >&2
+        exit 1
+    fi
+    sleep 1
 done
 echo "INFO: reached Cassandra on '$cass_ip:$cass_port' after '$attempts' attemps." \
-	 "Waiting 10 more seconds for the service to be available." >&2
-sleep 10
+     "Waiting 30 more seconds for the service to be available." >&2
+sleep 30
 
-if ${CLIENT_SSL} == "true"
-then
- /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc"  -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p cassandra --ssl 2>&1>/dev/null
- if [[ "$?" == 1 ]]; then
-  /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc"  -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p $CASS_PWD --ssl 2>&1>/dev/null
- fi
- /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc"  -e "alter keyspace system_auth WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true" -u cassandra -p $CASS_PWD --ssl  2>&1>/dev/null
- /var/vcap/jobs/cassandra_seed/bin/./node-tool.sh repair system_auth
-#exit 0
-else
-	echo "converging password, attempt 1: use default password" >&2
-    /var/vcap/packages/cassandra/bin/cqlsh `hostname -I` -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p cassandra
-    if [[ "$?" == 1 ]]; then
-		echo "converging password, attempt 2: use new password" >&2
-        /var/vcap/packages/cassandra/bin/cqlsh `hostname -I` -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p $CASS_PWD
-    fi
-    /var/vcap/packages/cassandra/bin/cqlsh `hostname -I` -e "alter keyspace system_auth WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true" -u cassandra -p $CASS_PWD
-    /var/vcap/jobs/cassandra_seed/bin/./node-tool.sh repair system_auth
+echo "converging password, attempt 1: use default password" >&2
+/var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc" \
+    -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p cassandra
+failure=$?
+echo "attempt 1 exit status: '$failure'" >&2
+
+if [[ "$failure" == 1 ]]; then
+    echo "converging password, attempt 2: use new password" >&2
+    /var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc" \
+        -e "alter role cassandra with password = '$CASS_PWD' " -u cassandra -p $CASS_PWD
+    echo "attempt 2 exit status: '$?'" >&2
 fi
 
-/var/vcap/jobs/cassandra_seed/bin/./creer_pem_cli_serv.sh
+/var/vcap/packages/cassandra/bin/cqlsh --cqlshrc "/var/vcap/jobs/cassandra_seed/root/.cassandra/cqlshrc" \
+    -e "alter keyspace system_auth WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'}  AND durable_writes = true" -u cassandra -p $CASS_PWD
+
+/var/vcap/jobs/cassandra_seed/bin/node-tool.sh repair system_auth
+
+/var/vcap/jobs/cassandra_seed/bin/creer_pem_cli_serv.sh
 exit 0
