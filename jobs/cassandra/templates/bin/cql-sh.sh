9cc12fc63dad46b0c6551caf122804221489449c
Omer MISSOUM <omer.missoum@orange.com>
2017-11-22 14:40:33 +0000

    fixing a typo on cql-sh.sh script

--- jobs/cassandra/templates/bin/cql-sh.sh
+++ jobs/cassandra/templates/bin/cql-sh.sh
@@ -6,7 +6,7 @@ set -u # report the usage of uninitialized variables.
 export LANG=en_US.UTF-8
 
 exec chpst -u vcap:vcap \
-    env HOME=/home/vcap
+    env HOME=/home/vcap \
         /var/vcap/packages/cassandra/bin/cqlsh \
             --cqlshrc /var/vcap/jobs/cassandra/root/.cassandra/cqlshrc \
             "$@"
