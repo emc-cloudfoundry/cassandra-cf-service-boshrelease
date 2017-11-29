7e16c717e2085448ee4372bdb82ef7dd07cccd53
Christophe LE COZ <christophe.lecoz@orange.com>
2017-11-29 13:35:26 +0000

    Add jmx_prometheus_javaagent-0.1.0.jar

--- downloadblob.sh
+++ downloadblob.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
 wget http://apache.trisect.eu/cassandra/3.9/apache-cassandra-3.9-bin.tar.gz
 wget https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.30.0/cf-cli_6.30.0_linux_x86-64.tgz
 git clone https://github.com/JCL38-ORANGE/cf-cassandra-example-app.git
