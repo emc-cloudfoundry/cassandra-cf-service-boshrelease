33952d47137f9e86779e1eb83a49ec2237acc4c4
Benjamin Gandon <bgandon.ext@orange.com>
2018-02-09 12:07:42 +0100

    Simplify the 'cqlshrc' config for the 'cassandra' job + Restrict access to it

--- jobs/cassandra/templates/bin/pre-start.sh
+++ jobs/cassandra/templates/bin/pre-start.sh
@@ -7,3 +7,7 @@ echo "Deactivating Linux swap"
 /sbin/swapoff --all
 echo "Deactivated Linux swap"
 <% end %>
+
+
+# cqlshrc is only accessed by the 'root' user from the 'post-start' script
+chmod 600 /var/vcap/jobs/cassandra/root/.cassandra/cqlshrc
